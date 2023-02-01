a17d8adb76b52902b78db21401910ce88adf5d28
Andrew Godwin <hello@andrewgodwin.dev>
2023-02-01 15:03:54 -0800

    Render basic form

--- src/components/Game/GuessInput.js
+++ src/components/Game/GuessInput.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-function GuessInput() {}
+function GuessInput() {
+  return (
+    <form className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input id="guess-input" type="text" />
+    </form>
+  );
+}
 
 export default GuessInput;
