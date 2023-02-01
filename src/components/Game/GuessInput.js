284e5d75e41bb8cee129748c09b1076e76d54f4b
Andrew Godwin <hello@andrewgodwin.dev>
2023-02-01 15:06:56 -0800

    Control text input with React state

--- src/components/Game/GuessInput.js
+++ src/components/Game/GuessInput.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
   return (
     <form className="guess-input-wrapper">
       <label htmlFor="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" />
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => {
+          setGuess(event.target.value);
+        }}
+      />
     </form>
   );
 }
