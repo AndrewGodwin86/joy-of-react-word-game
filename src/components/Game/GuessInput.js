58dc398452e7cc8effc58dbe16e6193168895d1b
Andrew Godwin <hello@andrewgodwin.dev>
2023-02-01 15:13:29 -0800

    When form is submitted log guess and clear input

--- src/components/Game/GuessInput.js
+++ src/components/Game/GuessInput.js
@@ -3,8 +3,15 @@ import React from "react";
 function GuessInput() {
   const [guess, setGuess] = React.useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ guess });
+
+    setGuess("");
+  }
+
   return (
-    <form className="guess-input-wrapper">
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
