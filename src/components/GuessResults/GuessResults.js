dcdf85fff3f0a3102a688d50186ff7eb0caa7ba2
Andrew Godwin <hello@andrewgodwin.dev>
2023-02-02 11:16:50 -0800

    Add grid squares for guesses

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,6 +3,41 @@ import React from "react";
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
+      <p className="guess">
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+      </p>
+      <p className="guess">
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+      </p>
+      <p className="guess">
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+      </p>
+      <p className="guess">
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+      </p>
+      <p className="guess">
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+        <span className="cell"></span>
+      </p>
       {guesses.map((guess, index) => (
         <p key={index} className="guess">
           {guess}
